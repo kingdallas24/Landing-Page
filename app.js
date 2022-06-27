6b905eba159f86265023e046a392e08799103555
Miles Biringanine <milesgabriel23@gmail.com>
2022-06-27 10:58:43 -0500

    images

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+// Grab important elements.
 const inputs = document.querySelectorAll("input");
-const submitButton = document.querySelector("button");
-const form = document.querySelector("form");
+const form = document.querySelector("#form");
+const firstname = document.querySelector("#firstName");
+const lastname = document.querySelector("#lastName");
 
+// Create sign up requirements
 const patterns = {
   firstName: /^([a-z])+$/i,
   lastName: /^([a-z])+$/i,
@@ -11,6 +14,7 @@ const patterns = {
     /^(?=.*[A-Z])(?=.*[a-z])(?=.+[\d])(?=.*[~!@#$%^&*()-_+=])[a-zA-z\d~!@#$%^&*()-_+=]{8,}$/,
 };
 
+// Create a function that validates the regex requirements and changes class name of each input field based on results
 function validate(field, regex) {
   if (regex.test(field.value)) {
     field.className = "valid";
@@ -19,8 +23,27 @@ function validate(field, regex) {
   }
 }
 
+// For each input field add an event listener. When the event listener is activated the validate function will run.
 inputs.forEach((input) => {
   input.addEventListener("keyup", (e) => {
     validate(e.target, patterns[e.target.attributes.name.value]);
   });
+});
+
+// if the submit button is clicked and failed. Show all empty input fields and errors. Otherwise proceed.
+
+// function displayGreeting() {
+//   const para = document.createElement("h2");
+//   document.body.appendChild(para);
+//   para.textContent = `Thank you ${username}`;
+// }
+
+// input.addEventListener("change", () => {
+//   displayGreeting();
+// });
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const newDiv = document.createElement("div");
+  console.log(`Thank you ${firstname.value} ${lastname.value}`);
 });
